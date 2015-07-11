4e09d2ea1313bc8c9a0fc6cf6d7306aaffbb4a63
Luigi Vincent <luigipvincent@gmail.com>
2015-07-11 11:52:01 -0400

    add DiplomaGenerator.java

---
+++ DiplomaGenerator.java
@@ -0,0 +1,27 @@
+/* Author: Luigi Vincent
+
+*/
+
+
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class DiplomaGenerator extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+
+		//stage.setScene();
+		stage.show();
+	}
+}
